8a7cc8a6f08ba4e7e0f271be9474ff385a8aca38
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-05-15 02:50:41 -0700

    add EventEmitter

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
-const tutorial = require('./tutorial');
-console.log(tutorial);
-console.log(tutorial.sum(1, 1));
-console.log(tutorial.PI);
-console.log(new tutorial.SomeMathObject());
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
