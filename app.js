637512accd8d1ce02863a922c9c6ed70e524f706
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-05-15 20:53:29 -0700

    Globals

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
-const EventEmitter = require('events');
-const eventEmitter = new EventEmitter();
+// Globals:
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use modules 
+// module - info about current module 
+// process - info about env where the program is being executed
 
-eventEmitter.on('tutorial', (num1, num2) => {
-    console.log(num1 + num2);
-});
-
-eventEmitter.emit('tutorial', 1, 2);
-
-class Person extends EventEmitter {
-    constructor(name) {
-        super();
-        this._name = name;
-    }
-
-    get name() {
-        return this._name;
-    }
-}
-
-let pedro = new Person('Pedro');
-pedro.on('name', () => { console.log("my name is " + pedro.name); });
-
-let christina = new Person("Christina");
-christina.on('name', () => { console.log('my name is ' + christina.name) });
-
-christina.emit("name");
-pedro.emit('name');
+console.log(__dirname);
+console.log(__filename);
