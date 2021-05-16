ea5c567ebab4b30f2e9cdf2097af63d773574808
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-05-15 23:58:13 -0700

    write to file

--- app.js
+++ app.js
@@ -5,4 +5,9 @@ const { readFileSync, writeFileSync } = require('fs')
 //fs.readFileSync
 //fs.writeFileSync
 
-const first = readFileSync('./data')
+const first = readFileSync('./data/first.txt', 'utf8')
+const second = readFileSync('./data/second.txt', 'utf8')
+
+console.log(first, second)
+
+writeFileSync('./data/result.txt', `Here is the result : ${first} , ${second}`)
