2116dbfb19f375801df2c8728bfc490a140aa15f
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-05-17 22:02:11 -0700

    set up server

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 
-    res.write('Welcome to our home page');
-    res.end();
+    console.log("Req: \n")
+    console.log(req);
 
+    if (req.url === '/') {
+        res.write('Welcome to our home page');
+        res.end();
+        //res.end('Welcome to our home page');
+    }
+
+    if (req.url === '/about') {
+        res.end('Here is our short history');
+    }
+
+    res.end(`<h1>Oops!</h1>
+    <p>We can't seem to find the page you are looking for</p>
+    <a href="/">back home</a>`);
 });
 
 server.listen(5000);
