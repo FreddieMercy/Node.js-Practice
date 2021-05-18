bf7687d865826c80adc476b1b8b592dc70ab15e5
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-05-18 13:45:21 -0700

    setTimeOut, setInterval, process.nextTick()

--- app.js
+++ app.js
@@ -1,28 +1,19 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-
-    console.log("Req: \n")
-    console.log(req);
-
-    if (req.url === '/') {
-        res.write('Welcome to our home page');
-        res.end();
-        //res.end('Welcome to our home page');
-    }
-
-    if (req.url === '/about') {
-        res.end('Here is our short history');
-    }
-
-    res.end(`<h1>Oops!</h1>
-    <p>We can't seem to find the page you are looking for</p>
-    <a href="/">back home</a>`);
+    res.write('Welcome to our home page');
+    res.end();
 });
 
 server.listen(5000);
 
-//TODO: setTimeOut, setInterval, process.nextTick()
+setTimeout(() => {
+    console.log("have waited 20 secs, now repeatedly run the callback function");
+    setInterval(() => {
+        console.log("run every 1 sec");
+    }, 1000)
+}, 20000);
+
 //TODO: new Promise()
 //TODO: then(), catch()
 //TODO: async() & await
