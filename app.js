b7e726ba76fe51f2a896fb1e4b65816c1a446631
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-05-18 23:16:22 -0700

    async func

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+// 'async' allows using 'await' inside of it
 async function hello() {
     // return await Promise.resolve("Hello");
     return greeting = await Promise.resolve("Hello"); // whatever, async function always return 'Promise'
 };
 
+// hello().then((msg) => { console.log(msg) });
 hello().then(console.log);
 
 //TODO: require('util').promisify()
