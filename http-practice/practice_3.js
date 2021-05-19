d808d65b2e663fcd6ad56f9900ae3f9fd7bc9def
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-05-18 23:17:13 -0700

    practice_3.js

---
+++ http-practice/practice_3.js
@@ -0,0 +1,8 @@
+// 'async' allows using 'await' inside of it
+async function hello() {
+    // return await Promise.resolve("Hello");
+    return greeting = await Promise.resolve("Hello"); // whatever, async function always return 'Promise'
+};
+
+// hello().then((msg) => { console.log(msg) });
+hello().then(console.log);
