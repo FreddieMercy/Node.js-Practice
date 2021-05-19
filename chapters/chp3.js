6bcfce083d2fc2b11af5059df9892fe4c680ded4
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-05-18 23:46:29 -0700

    it is string compare to integer, thus use == not ===

--- chapters/chp3.js
+++ chapters/chp3.js
@@ -9,17 +9,18 @@ let num1 = Math.floor((Math.random() * 10) + 1);
 let num2 = Math.floor((Math.random() * 10) + 1);
 let answer = num1 + num2;
 
-rl.question(`What is ${num1} + ${num2}? \n`, (userInput) => {
-    console.log(userInput);
-    if (userInput.trim() === answer) {
+rl.question(`What is ${num1} + ${num2}? (answer is ${answer}) \n`, (userInput) => {
+    console.log("User input: " + userInput.trim());
+    if (userInput.trim() == answer) { // it is string compare to integer, thus use == not ===
+        console.log("Correct!!!")
         rl.close();
     }
     else {
-        rl.setPrompt('Incorrect! Please try again: What is ${num1} + ${num2}?');
+        rl.setPrompt(`Incorrect! Please try again: What is ${num1} + ${num2}?`);
         rl.prompt();
     }
 });
 
 rl.on('close', () => {
-    console.log("Correct!!!");
+    console.log("Closed...");
 })
